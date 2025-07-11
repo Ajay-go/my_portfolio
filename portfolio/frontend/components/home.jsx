ad3475201dcd566af90b93b1d8ea9ef898353534
ajay gour <ajaygour3333@gmail.com>
2025-07-11 20:43:13 +0530

    some update

---
+++ portfolio/frontend/components/home.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import './home.css'
+function Home() {
+
+    return <>
+
+        <div>
+            <div id="sidebar">
+
+                <div id="list">
+                    <ul>
+                        <li>about</li>
+                        <li>skills</li>
+                        <li>education</li>
+                        <li>projects</li>
+                        <li>contact</li>
+                    </ul>
+                </div>
+            </div>
+            <div id="chat_with_ai">
+
+            </div>
+
+        </div>
+
+    </>
+}
+export default Home;
